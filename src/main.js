193a19deb6f810677587e600ae7754104f8215a1
Cleiton Arcanjo <152240025+CleitonArcanj0@users.noreply.github.com>
2025-12-30 00:31:21 -0300

    feat: retomar contagem após pausa
    Agora o cronômetro continua de onde parou quando o usuário clica em start
    após ter pausado. Isso evita reiniciar os 25 minutos toda vez que pausar.

--- src/main.js
+++ src/main.js
@@ -1,40 +1,74 @@
 const campoMin = window.document.getElementById("minutos");
 const campoSeg = window.document.getElementById("segundos");
-let tempo;
+let tempo
+let minutos 
+let segundos
+let rodando = false
+let qtd_start_ativado = 0
 
-function start() {
-    let minutos = 24;
-    let segundos = 59;
+const start = () => {
+    if (qtd_start_ativado === 0) {
+        minutos = 24
+        segundos = 59
+        qtd_start_ativado++
+    } else if (qtd_start_ativado > 0 && rodando === false) {
+        rodando = true
+        minutos = Number(window.document.getElementById("minutos").innerHTML)
+        segundos = Number(window.document.getElementById("segundos").innerHTML)
+        temp(minutos, segundos)
+    }
 
     //impede que o usuário crie vários intervalos
-    if(tempo){
-        return;
+    if (tempo) {
+        return
     }
-    tempo = setInterval(function () {
-        campoMin.innerText = `${minutos.toString().padStart(2, "0")}`
-        campoSeg.innerText = `${segundos.toString().padStart(2, "0")}`
-        if (segundos !== 0) {
-            segundos--;
-        } else {
-            if (minutos === 0) {
-                alert("Acabou o tempo!");
-                resetar()
+    rodando = true
+    temp(minutos, segundos)
+
+}
+
+const temp = (minutos, segundos) => {
+    if (rodando === true) {
+
+        tempo = setInterval(function () {
+
+            campoMin.innerText = `${minutos.toString().padStart(2, "0")}`
+            campoSeg.innerText = `${segundos.toString().padStart(2, "0")}`
+
+            if (segundos !== 0) {
+                segundos--;
+
             } else {
-                minutos--;
-                segundos = 59;
+
+                if (minutos === 0) {
+                    alert("Acabou o tempo!");
+                    qtd_start_ativado = 0
+                    resetar()
+
+                } else {
+                    minutos--;
+                    segundos = 59;
+                }
+
             }
-        }
-    }, 1000);
+
+
+        }, 1000);
+
+    }
 
 }
 
-function parar() {
+
+const parar = () => {
     clearInterval(tempo)
+    rodando = false
 }
 
-function resetar() {
+const resetar = () => {
+    parar()
+    tempo = null
+    qtd_start_ativado = 0
     campoMin.innerText = "25"
     campoSeg.innerText = "00"
-    tempo = null;
-    parar()
 }
